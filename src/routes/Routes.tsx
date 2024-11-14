91dc0c35c76b94e710fe9341ace1d7a6dd5b7f90
kiyeong <kjr07145@gmail.com>
2024-11-15 02:27:17 +0900

    ci: 배포 테스트

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "../pages/MainPage/MainPage"; // 메인 페이지 컴포넌트 가져오기
 import StartPage from "../pages/Start/StartPage";
-import BookPage from "../pages/Book/BookPage";
+import BookPage from "../pages/book/BookPage";
 import LoadingPage from "../pages/LoadingPage/LoadingPage"; // 로딩 페이지 컴포넌트 가져오기/ 메인 페이지 컴포넌트 가져오기
 import AnalysisPage from "../pages/Analysis/AnalysisPage";
 import NotFoundPage from "../pages/NotFound/NotFoundPage";
