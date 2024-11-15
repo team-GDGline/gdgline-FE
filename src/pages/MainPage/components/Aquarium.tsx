dbe9475d690d2a4c74ac07541aa655c462684591
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-15 13:34:05 +0900

    feat: 아쿠아리움 물고기 fetch 적용

--- src/pages/MainPage/components/Aquarium.tsx
+++ src/pages/MainPage/components/Aquarium.tsx
@@ -1,7 +1,9 @@
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { keyframes } from "@emotion/react";
+import axios from "axios";
+
 import background_aquarium from "../../../assets/background_aquarium.svg";
-// 개별적으로 이미지를 import
 import gardenEel from "../../../assets/pixel/gardenEel.svg";
 import napoleonWrasse from "../../../assets/pixel/napoleonWrasse.svg";
 import arowana from "../../../assets/pixel/arowana.svg";
@@ -16,94 +18,86 @@ import clownfish from "../../../assets/pixel/clownfish.svg";
 import blackStingray from "../../../assets/pixel/blackStingray.svg";
 import leatherbackSeaTurtle from "../../../assets/pixel/leatherbackSeaTurtle.svg";
 import humboldtPenguin from "../../../assets/pixel/humboldtPenguin.svg";
+import { API_BASE_URL } from "../../../api/constant";
 
-// 이미지 배열 생성
-// 이미지와 위치 정보를 포함한 객체 배열
 const fishPixel = [
-  { src: gardenEel, top: "75%", left: "10%", width: "80px", height: "80px" },
-  {
-    src: napoleonWrasse,
-    top: "45%",
-    left: "80%",
-    width: "60px",
-    height: "60px",
-  },
-  { src: arowana, top: "40%", left: "5%", width: "60px", height: "60px" },
-  {
-    src: blacktipReefShark,
-    top: "5%",
-    left: "60%",
-    width: "140px",
-    height: "140px",
-  },
-  {
-    src: africanManatee,
-    top: "45%",
-    left: "30%",
-    width: "80px",
-    height: "80px",
-  },
-  { src: giantGrouper, top: "2%", left: "80%", width: "80px", height: "80px" },
-  {
-    src: smallClawedOtter,
-    top: "75%",
-    left: "30%",
-    width: "80px",
-    height: "80px",
-  },
-  { src: piranha, top: "35%", left: "55%", width: "50px", height: "50px" },
-  { src: zebraShark, top: "15%", left: "10%", width: "100px", height: "100px" },
-  {
-    src: californiaSeaLion,
-    top: "75%",
-    left: "55%",
-    width: "80px",
-    height: "80px",
-  },
-  { src: clownfish, top: "65%", left: "5%", width: "80px", height: "80px" },
-  {
-    src: blackStingray,
-    top: "5%",
-    left: "30%",
-    width: "110px",
-    height: "110px",
-  },
-  {
-    src: leatherbackSeaTurtle,
-    top: "55%",
-    left: "65%",
-    width: "80px",
-    height: "80px",
-  },
-  {
-    src: humboldtPenguin,
-    top: "70%",
-    left: "80%",
-    width: "80px",
-    height: "80px",
-  },
+  { src: gardenEel, key: "gardenEel", top: "75%", left: "10%", width: "80px", height: "80px" },
+  { src: napoleonWrasse, key: "napoleonWrasse", top: "45%", left: "80%", width: "60px", height: "60px" },
+  { src: arowana, key: "arowana", top: "40%", left: "5%", width: "60px", height: "60px" },
+  { src: blacktipReefShark, key: "blacktipReefShark", top: "5%", left: "60%", width: "140px", height: "140px" },
+  { src: africanManatee, key: "africanManatee", top: "45%", left: "30%", width: "80px", height: "80px" },
+  { src: giantGrouper, key: "giantGrouper", top: "2%", left: "80%", width: "80px", height: "80px" },
+  { src: smallClawedOtter, key: "smallClawedOtter", top: "75%", left: "30%", width: "80px", height: "80px" },
+  { src: piranha, key: "piranha", top: "35%", left: "55%", width: "50px", height: "50px" },
+  { src: zebraShark, key: "zebraShark", top: "15%", left: "10%", width: "100px", height: "100px" },
+  { src: californiaSeaLion, key: "californiaSeaLion", top: "75%", left: "55%", width: "80px", height: "80px" },
+  { src: clownfish, key: "clownfish", top: "65%", left: "5%", width: "80px", height: "80px" },
+  { src: blackStingray, key: "blackStingray", top: "5%", left: "30%", width: "110px", height: "110px" },
+  { src: leatherbackSeaTurtle, key: "leatherbackSeaTurtle", top: "55%", left: "65%", width: "80px", height: "80px" },
+  { src: humboldtPenguin, key: "humboldtPenguin", top: "70%", left: "80%", width: "80px", height: "80px" },
 ];
 
 interface AquariumProps {
-  children?: React.ReactNode; // children을 선택적 속성으로 변경
+  children?: React.ReactNode;
 }
 
 const Aquarium: React.FC<AquariumProps> = ({ children }) => {
+  const [fishData, setFishData] = useState<Record<string, boolean>>({});
+
+  useEffect(() => {
+    const fetchFishData = async () => {
+      try {
+        // const response = await axios.get(`${API_BASE_URL}/api/v1/pokedex`);
+        // setFishData(response.data); // 물고기 데이터 저장
+        setFishData(
+          {
+            "gardenEel": true,
+            "napoleonWrasse": false,
+            "arowana": true,
+            "blacktipReefShark": false,
+            "africanManatee": true,
+            "giantGrouper": false,
+            "smallClawedOtter": true,
+            "piranha": false,
+            "zebraShark": true,
+            "californiaSeaLion": true,
+            "clownfish": false,
+            "blackStingray": true,
+            "leatherbackSeaTurtle": false,
+            "humboldtPenguin": true
+          }
+        )
+      } catch (error) {
+        console.error("물고기 데이터를 가져오는 중 오류가 발생했습니다:", error);
+      }
+    };
+
+    fetchFishData();
+  }, []);
+
   return (
     <AquariumWrapper>
-      {fishPixel.map((fish, index) => (
-        <FishPixel
-          key={index}
-          src={fish.src}
-          alt={`fish-${index}`}
-          style={{
-            top: fish.top,
-            left: fish.left,
-            width: fish.width,
-            height: fish.height,
-          }}
-        />
-      ))}
+      {Object.entries(fishData).map(([key, value]) => {
+        if (!value) return null; // 잡히지 않은 물고기는 표시하지 않음
+
+        // 잡힌 물고기의 정보를 fishPixel 배열에서 찾음
+        const fishInfo = fishPixel.find(fish => fish.key === key);
+        if (!fishInfo) return null;
+
+        return (
+          <FishPixel
+            key={key}
+            src={fishInfo.src}
+            alt={`fish-${key}`}
+            style={{
+              top: fishInfo.top,
+              left: fishInfo.left,
+              width: fishInfo.width,
+              height: fishInfo.height,
+            }}
+          />
+        );
+      })}
       {children}
     </AquariumWrapper>
   );
@@ -120,7 +114,6 @@ const AquariumWrapper = styled.div`
   overflow: hidden;
 `;
 
-// 랜덤하게 움직이는 픽셀 아트 스타일 애니메이션
 const floatAnimation = (x: number, y: number) => keyframes`
   0% { transform: translate(0, 0); }
   50% { transform: translate(${x}px, ${y}px); }
@@ -130,7 +123,6 @@ const floatAnimation = (x: number, y: number) => keyframes`
 const FishPixel = styled.img`
   position: absolute;
   animation: ${() =>
-      floatAnimation(Math.random() * 20 - 10, Math.random() * 20 - 10)}
-    3s ease-in-out infinite;
-  image-rendering: pixelated; /* 픽셀 아트 효과 */
+    floatAnimation(Math.random() * 20 - 10, Math.random() * 20 - 10)} 3s ease-in-out infinite;
+  image-rendering: pixelated;
 `;
