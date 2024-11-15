fc3bba488c534c5d9e04164a18a30fd8d2f72ea7
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-15 23:10:18 +0900

    feat: accessToken 없을 싱에 loingPage로 navigate 구현

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -1,4 +1,1 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-export const ACCESS_TOKEN = "accessToken";
-export const REFRESH_TOKEN = "refreshToken";
--- src/routes/path.ts
+++ src/routes/path.ts
@@ -1,4 +1,5 @@
 export const RouterPath = {
+  token: "",
   root: "/",
   start: "/",
   login: "/login",
